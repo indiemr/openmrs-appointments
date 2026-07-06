688cdea024d337770ced48c98277fcded97e16cf
Senthil Athiban <senthilathiban0@gmail.com>
2026-07-06 19:17:14 +0530

    fix: run_calendar_event_listener_on_new_thread

--- api/src/main/java/org/openmrs/module/appointments/events/eventListener/AppointmentCalendarEventListener.java
+++ api/src/main/java/org/openmrs/module/appointments/events/eventListener/AppointmentCalendarEventListener.java
@@ -1,13 +1,18 @@
 package org.openmrs.module.appointments.events.eventListener;
 
+import java.util.concurrent.Executor;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.events.AppointmentBookingEvent;
 import org.openmrs.module.appointments.events.AppointmentEventType;
 import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.model.AppointmentStatus;
 import org.openmrs.module.appointments.service.AppointmentCalendarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -20,16 +25,17 @@ public class AppointmentCalendarEventListener {
     @Autowired
     private AppointmentCalendarService appointmentCalendarService;
 
+    @Autowired
+    @Qualifier("AppointmentsAsyncThreadExecutor")
+    private Executor executor;
+
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onAppointmentCreated(AppointmentBookingEvent event) {
         if (event.eventType != AppointmentEventType.BAHMNI_APPOINTMENT_CREATED) {
             return;
         }
-        try {
-            appointmentCalendarService.createCalendarEventForAppointment(event.getAppointment());
-        } catch (Exception e) {
-            log.error("Failed to sync calendar event on appointment create " + event.getAppointment().getUuid(), e);
-        }
+        executor.execute(() -> runWithContext(event, () ->
+        appointmentCalendarService.createCalendarEventForAppointment(event.getAppointment())));
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -37,16 +43,25 @@ public class AppointmentCalendarEventListener {
         if (event.eventType != AppointmentEventType.BAHMNI_APPOINTMENT_UPDATED) {
             return;
         }
-
-        Appointment appointment = event.getAppointment();
-        try {
+        executor.execute(() -> runWithContext(event, () -> {
+            Appointment appointment = event.getAppointment();
             if (AppointmentStatus.Cancelled.equals(appointment.getStatus())) {
                 appointmentCalendarService.cancelCalendarEventForAppointment(appointment);
             } else {
                 appointmentCalendarService.updateCalendarEventForAppointment(appointment);
             }
+        }));
+    }
+
+    private void runWithContext(AppointmentBookingEvent event, Runnable action) {
+        try {
+            Context.openSession();
+            Context.setUserContext(event.userContext);
+            action.run();
         } catch (Exception e) {
-            log.error("Failed to sync calendar event on appointment update " + appointment.getUuid(), e);
+            log.error("Failed to sync calendar for " + event.getAppointment().getUuid(), e);
+        } finally {
+            Context.closeSession();
         }
     }
 }
